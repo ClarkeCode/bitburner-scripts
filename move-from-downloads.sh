056874eb9a6d38a235d4844e9eaeda05cdcadcc1
Robert Clarke <clarke.john.robert@gmail.com>
2021-12-20 10:02:18 -0500

    Added a note on how to ignore previously-committed files

--- move-from-downloads.sh
+++ move-from-downloads.sh
@@ -2,6 +2,10 @@
 
 #NOTE: This file is excluded by the .gitignore, so your user directory
 #      will not be included in your commits
+#
+#To have git ignore an already-commited file:
+#      Run git update-index --assume-unchanged move-from-downloads.sh
+#      If this file appears in 'git status' after changing the DL_PATH
 DL_PATH=/c/Users/<Your User Goes Here>/Downloads/
 FILE_PATH="$DL_PATH"bitburnerScripts.zip
 [[ -f "$FILE_PATH" ]] && mv "$FILE_PATH" .
