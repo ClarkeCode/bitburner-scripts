fdaf5ce8eb730ee5861ff2a0ba9c37cbdb4ded32
Robert Clarke <clarke.john.robert@gmail.com>
2022-01-08 06:21:03 -0500

    Tweak to filtering behaviour

--- scripts/webcrawler.js
+++ scripts/webcrawler.js
@@ -10,7 +10,7 @@ export async function main(ns) {
 	// ns.tail();
 	// ns.disableLog("ALL");
 	var minLevel = ns.args[0] == undefined ? 1 : ns.args[0];
-	var maxLevel = ns.args[1] == undefined ? 99999999 : ns.args[1];
+	var maxLevel = ns.args[1] == undefined ? (ns.getHackingLevel() < 1000 ? ns.getHackingLevel()+100 : 99999999) : ns.args[1];
 	ns.tprintf("\n\nWEBCRAWLER -> START");
 
 	var servers = (await allServers(ns)).sort((a, b) => a.requiredHackingSkill - b.requiredHackingSkill).filter(
