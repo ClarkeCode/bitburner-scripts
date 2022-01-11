8921294233bf564ae1398f45d01296e0a20ce88f
Robert Clarke <clarke.john.robert@gmail.com>
2022-01-10 22:15:59 -0500

    Basic scripts can now delay activation for batching

--- scripts/util/crippler.js
+++ scripts/util/crippler.js
@@ -4,14 +4,16 @@
 export async function main(ns) {
 	var fl = ns.flags([
 		["loop", false],
+		["delay", 0],
+		["tail", false],
 		["stock", false]
 	]);
 
 	var target = fl._[0];
 	if (target == undefined) { ns.tprint("ERROR: target not defined"); return; }
-	if (fl.loop) { ns.tail(); }
-
+	if (fl.tail) { ns.tail(); }
 	do {
+		if (fl.delay > 0) { await ns.sleep(fl.delay); }
 		await ns.weaken(target, {"stock": fl.stock});
 	} while (fl.loop);
 }
